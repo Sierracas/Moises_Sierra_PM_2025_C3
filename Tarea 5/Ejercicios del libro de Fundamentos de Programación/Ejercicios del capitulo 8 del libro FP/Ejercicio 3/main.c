341af799e2c5dd740b9a656c8b3acb1d6b3d267d
Sierracas <20220928@itla.edu.do>
2025-10-25 01:00:20 -0400

    Agregando parte final de la tarea 5

--- Tarea 5/Ejercicios del libro de Fundamentos de Programación/Ejercicios del capitulo 8 del libro FP/Ejercicio 3/main.c
+++ Tarea 5/Ejercicios del libro de Fundamentos de Programación/Ejercicios del capitulo 8 del libro FP/Ejercicio 3/main.c
@@ -15,7 +15,7 @@ struct empleado
     char nombre[20];
     char departamento[20];
     float sueldo;
-        dimicilio direccion;
+        domicilio direccion;
 };
 
 void Lectura(struct empleado *a)
@@ -24,7 +24,7 @@ void Lectura(struct empleado *a)
     gets(a->nombre);
     fflush(stdin);
     printf("Ingrese el departamento de la empresa: ");
-    gtes(a->departamento);
+    gets(a->departamento);
     printf("Ingrese el sueldo del empleado: ");
     scanf("%f", &a->sueldo);
     fflush(stdin);
@@ -42,5 +42,54 @@ void Lectura(struct empleado *a)
 
 void main(void)
 {
+struct empleado e0 = {"Arturo", "Compras", 15500.75, "San Jeronimo", 120, 3490, "Toluca"};
+struct empleado *e1, *e2, e3, e4;
 
+e1 = (struct empleado *)malloc(sizeof(struct empleado));
+printf("\nIngrese el nombre del empleado 1: ");
+scanf("%S", &(*e1).nombre);
+fflush(stdin);
+printf("Ingrese el departamento de la empresa: ");
+gets(e1->departamento);
+printf("Ingrese el sueldo del empleado: ");
+scanf("%f", &e1->sueldo);
+printf("--Ingrese la direccion del emepleado--");
+printf("\n\tCalle: ");
+fflush(stdin);
+gets(e1->direccion.calle);
+printf("\tNumero: ");
+scanf("%d", &e1->direccion.numero);
+printf("\tCodigo Postal: ");
+scanf("%d", &e1->direccion.cp);
+printf("\tLocalidad ");
+fflush(stdin);
+gets(e1->direccion.localidad);
+
+printf("\nIngrese el nombre del empleado 3: ");
+scanf("%S", &e3.nombre);
+fflush(stdin);
+printf("Ingrese el departamento de la empresa: ");
+gets(e3.departamento);
+printf("Ingrese el sueldo del empleado: ");
+scanf("%f", &e3.sueldo);
+printf("--Ingrese la direccion del empleado--");
+printf("\n\tCalle: ");
+fflush(stdin);
+gets(e3.direccion.calle);
+printf("\tNumero: ");
+scanf("%d", &e3.direccion.cp);
+printf("\tLocalidad: ");
+fflush(stdin);
+gets(e3.direccion.localidad);
+
+e2 = (struct empleado *)malloc(sizeof(struct empleado));
+Lectura(e2);
+
+Lectura(&e4);
+
+printf("\nDatos del empleado 1\n");
+printf("%S\t%s\t%.2f\t%s\t%d\t%d\t%s", e1->nombre, e1->departamento, e1->sueldo, e1->direccion.calle, e1->direccion.numero, e1->direccion.cp, e1->direccion.localidad);
+
+printf("\nDatos del empleado 4n");
+printf("%S\t%s\t%.2f\t%s\t%d\t%d\t%s", e4.nombre, e4.departamento, e4.sueldo, e4.direccion.calle, e4.direccion.numero, e4.direccion.cp, e4.direccion.localidad);
 }
